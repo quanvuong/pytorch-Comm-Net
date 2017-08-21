a68abde7f90cd5989dc237c96b1b7615b255c1cc
Quan Vuong <quan.vuong@nyu.edu>
2017-08-21 22:55:47 +0800

    parse args and pass to main

---
+++ main.py
@@ -0,0 +1,28 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--num_levers', type=int, default=5)
+
+parser.add_argument('--num_agents', type=int, default=500)
+
+parser.add_argument('--num_agents_drawn', type=int, default=5)
+
+parser.add_argument('--num_comm_step', type=int, default=2)
+
+parser.add_argument('--hidden_state_size', type=int, default=128)
+
+parser.add_argument('--num_trials', type=int, default=500)
+
+parser.add_argument('--num_training_batch', type=int, default=50000)
+
+parser.add_argument('--batch_size', type=int, default=64)
+
+
+def main(args):
+    pass
+
+if __name__ == '__main__':
+
+    args = parser.parse_args()
+    main(args)
